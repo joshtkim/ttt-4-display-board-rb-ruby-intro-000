254f227c936d5596a5224f053f487f938d23977f
Joshua Kim <jykim92@gmail.com>
2020-03-28 14:19:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,12 @@
-def display_board
+def display_board (board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
-display_board
+display_board(board)
+
+
